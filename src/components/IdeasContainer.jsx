e3d681fd60eb9d23c7f7f3147c78e457a06017fb
Paul Traue <ptrauejr@gmail.com>
2017-12-08 16:15:24 -0600

    Adds first data show capability to the IdeasComponent and some layout css

--- src/components/IdeasContainer.jsx
+++ src/components/IdeasContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class IdeasContainer extends Component {
     constructor(props) {
@@ -9,12 +10,26 @@ class IdeasContainer extends Component {
     }
 
     componentDidMount() {
+        axios.get('http://localhost:3001/api/v1/ideas')
+            .then(response => {
+                console.log(response)
+                this.setState({ideas: response.data})
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
         return (
-            <div className="">
-                Ideas
+            <div>
+                {this.state.ideas.map((idea) => {
+                    return (
+                        <div className="tile" key={idea.id}>
+                            <h4>{idea.title}</h4>
+                            <p>{idea.body}</p>
+                        </div>
+                    )
+
+                })}
             </div>
         );
     }
