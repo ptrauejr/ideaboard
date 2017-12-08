9791501e8f695e011c7d3dd3188f1de1c93b28ce
Paul Traue <ptrauejr@gmail.com>
2017-12-08 15:51:52 -0600

    Adds initial state to IdeasContainer

--- src/components/IdeasContainer.jsx
+++ src/components/IdeasContainer.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class IdeasContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ideas: []
+        }
+    }
+
+    componentDidMount() {
+    }
+
     render() {
         return (
             <div className="">
