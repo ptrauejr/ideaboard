fa26814060cbdb4b547a2840050320d86e86292f
Paul Traue <ptrauejr@gmail.com>
2017-12-09 17:36:55 -0600

    Adds the IdeaForm component

---
+++ src/components/IdeaForm.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class IdeaForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className="tile">
+                <form>
+                    <input className="input" type="text" name="title" placeholder="Enter a Title" />
+                    <textarea className='input' name='body' placeholder='Describe your idea'></textarea>t
+                </form>
+            </div>
+        )
+    }
+}
+
+export default IdeaForm
