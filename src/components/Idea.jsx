ced4f8b9e6c36ecc4cadd6ce61b56630aba93039
Paul Traue <ptrauejr@gmail.com>
2017-12-09 18:53:27 -0600

    Sets a click handler for the idea component

--- src/components/Idea.jsx
+++ src/components/Idea.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
-class Idea extends Compondent {
+class Idea extends Component {
+    handleClick = () => { this.props.onClick(this.props.idea.id) }
+
     render() {
-        <div className="tile">
-            <h4>{idea.title}</h4>
-            <p>{idea.body}</p>
-        </div>
+        return (
+            <div className="tile">
+                <h4 onClick={this.handleClick}>{this.props.idea.title}</h4>
+                <p onClick={this.handleClick}>{this.props.idea.body}</p>
+            </div>
+        )
     }
 }
 
