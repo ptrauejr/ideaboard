d89dcbaca15d2f0ab38d057a637b287beda30fc4
Paul Traue <ptrauejr@gmail.com>
2017-12-09 18:08:36 -0600

    Updates the backend system now when new ideas are added to the board

--- src/components/IdeaForm.jsx
+++ src/components/IdeaForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeaForm extends Component {
     constructor(props) {
@@ -10,10 +11,21 @@ class IdeaForm extends Component {
     }
 
     handleInput = (e) => {this.setState({[e.target.name]: e.target.value})}
+
+    handleBlur = () => {
+        const idea = { title: this.state.title, body: this.state.body }
+        axios.put(
+            `http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+            {idea: idea}
+        ).then(response => {
+            console.log(response)
+        }).catch(error => console.log(error))
+    }
+    
     render() {
         return (
             <div className="tile">
-                <form>
+                <form onBlur={this.handleBlur}>
                     <input className="input" type="text" name="title" placeholder="Enter a Title"
                            value={this.state.title} onChange={this.handleInput}/>
                     <textarea className='input' name='body' placeholder='Describe your idea'
